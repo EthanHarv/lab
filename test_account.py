10aad77d2168693e965198222522673d9566b2aa
EthanHarv <github@ethanharv.com>
2022-11-19 18:24:47 -0600

    Use approx for all comparisons (match return type)

--- test_account.py
+++ test_account.py
@@ -34,26 +34,26 @@ class Test:
         Accounts should have a name and balance of 0.
         '''
         assert self.a_1.get_name() == 'Foo'
-        assert self.a_1.get_balance() == 0
+        assert self.a_1.get_balance() == pytest.approx(0, abs=0.001)
 
         assert self.a_2.get_name() == 'Bar'
-        assert self.a_2.get_balance() == 0
+        assert self.a_2.get_balance() == pytest.approx(0, abs=0.001)
 
     def test_deposit(self):
         '''
         Test deposits
         '''
         # Basic functionality
-        assert self.a_1.get_balance() == 0
+        assert self.a_1.get_balance() == pytest.approx(0, abs=0.001)
         assert self.a_1.deposit(5) is True
-        assert self.a_1.get_balance() == 5
+        assert self.a_1.get_balance() == pytest.approx(5, abs=0.001)
         assert self.a_1.deposit(10.0) is True
         assert self.a_1.get_balance() == pytest.approx(15.0, abs=0.001)
 
         # Ensure accounts aren't at all connected.
-        assert self.a_2.get_balance() == 0
+        assert self.a_2.get_balance() == pytest.approx(0, abs=0.001)
         assert self.a_1.deposit(10) is True
-        assert self.a_2.get_balance() == 0
+        assert self.a_2.get_balance() == pytest.approx(0, abs=0.001)
 
         # Return false if negative, 0
         assert self.a_1.deposit(-1) is False
@@ -68,26 +68,26 @@ class Test:
         Test withdrawals
         '''
         # Basic functionality
-        assert self.a_1.get_balance() == 0
+        assert self.a_1.get_balance() == pytest.approx(0, abs=0.001)
         assert self.a_1.deposit(5) is True
-        assert self.a_1.get_balance() == 5
+        assert self.a_1.get_balance() == pytest.approx(5, abs=0.001)
         assert self.a_1.withdraw(5) is True
-        assert self.a_1.get_balance() == 0
+        assert self.a_1.get_balance() == pytest.approx(0, abs=0.001)
 
         assert self.a_1.deposit(10.0) is True
         assert self.a_1.withdraw(5) is True
         assert self.a_1.get_balance() == pytest.approx(5.0, abs=0.001)
 
         # Ensure accounts aren't at all connected.
-        assert self.a_2.get_balance() == 0
+        assert self.a_2.get_balance() == pytest.approx(0, abs=0.001)
         assert self.a_1.deposit(10) is True
         assert self.a_1.withdraw(5) is True
-        assert self.a_2.get_balance() == 0
+        assert self.a_2.get_balance() == pytest.approx(0, abs=0.001)
 
         # Return false if negative, 0, greater than account balance
         assert self.a_2.withdraw(-1) is False
         assert self.a_2.withdraw(0) is False
-        assert self.a_2.withdraw(1) is False
+        assert self.a_2.withdraw(1) is False # Account balance should currently be 0, so we are withdrawing more than the account has at this point.
 
         # Check incompatible type
         with pytest.raises(TypeError):
